0d3764c7cd1427cfe62fe8c44c73d65e3738a174
zhangxin <13622872688@163.com>
2023-02-05 15:17:06 +0800

    新增拖尾效果

--- assets/scripts/BaseAI.ts
+++ assets/scripts/BaseAI.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, RigidBody, Vec3, math, UITransform } from 'cc';
+import { _decorator, Component, Node, RigidBody, Vec3, math, UITransform, Animation, Sprite } from 'cc';
 import { Ball } from './Ball';
 const { ccclass, property, requireComponent } = _decorator;
 
@@ -9,23 +9,35 @@ export class BaseAI extends Ball {
     private _uiTransform: UITransform | null = null;
     private _originWidth = 0;
     private _originHeight = 0;
-
+    private _isPlayingMotionStreak = false;
+    @property(Animation)
+    motionStreakAnimation:Animation;
     start() {
         super.start();
-        this.move();
         this._uiTransform = this.getComponent(UITransform);
         this._originWidth = this._uiTransform.width;
         this._originHeight = this._uiTransform.height;
     }
-    move() {
-        // this._rigidBody.setLinearVelocity(this._speed);
-    }
+
     update(deltaTime: number) {
         this._rigidBody.getLinearVelocity(this._velocity);
         const length = this._velocity.length();
         if (length > 10) {
             this._uiTransform.width = this._originWidth * (0.5 + 5 / length);
-            this._uiTransform.height = this._originHeight * (0.5 + 5 / length);  
+            this._uiTransform.height = this._originHeight * (0.5 + 5 / length);
+            if (!this._isPlayingMotionStreak && this.motionStreakAnimation){
+                this.motionStreakAnimation.play();
+                this._isPlayingMotionStreak = true;
+                this.motionStreakAnimation.node.eulerAngles = new Vec3(this.motionStreakAnimation.node.eulerAngles.x, this.motionStreakAnimation.node.eulerAngles.y, Math.atan2(this._velocity.y, this._velocity.x) * (180 / Math.PI));
+
+            }
+            
+
+        } else {
+            if (this.motionStreakAnimation){
+                this.motionStreakAnimation.pause();
+                this.motionStreakAnimation.node.getComponent(Sprite).spriteFrame = null;
+            }
         }
     }
 }
