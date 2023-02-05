98b376c4bcf47e6ce295c7795df3c421f6494097
timlyeee <timelyeee@gmail.com>
2023-02-05 12:28:26 +0800

    stash

--- assets/scripts/UIManager.ts
+++ assets/scripts/UIManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, director, Label } from 'cc';
+import { _decorator, Component, Node, director, Label, Color } from 'cc';
 import { Ball } from './Ball';
 import { LevelManager } from './LevelManager';
 const { ccclass, property } = _decorator;
@@ -11,9 +11,24 @@ export class UIManager extends Component {
     @property(Node)
     public center: Node | null = null;
     public score = 0;
-
+    @property(Label)
+    public time: Label;
+    private _time = 60;
+    @property(Label)
+    public word: Label;
     start() {
-
+        this.schedule(()=>{
+            this._time--;
+            this.time.string = this._time.toString();
+            if(this._time<10){
+                //时间快结束了，更改Label的颜色。
+                this.time.color = Color.RED;
+            }
+            if(this._time == 0){
+                // GAME OVER
+                this.timeup()
+            }
+        } ,1);
     }
 
     update(deltaTime: number) {
@@ -27,11 +42,22 @@ export class UIManager extends Component {
         Ball.balls = [];
         director.loadScene('MainScene');
     }
-
+    timeup(){
+        if(this.score>60){
+            this.word.string = "病毒成功感染了宿主";
+            
+            //TODO: animtation for a better view
+        }
+        else {
+            this.word.string = "很遗憾，你失败了";
+        }
+        this.center.active = true;
+    }
     onDead() {
         this.center.active = true;
         LevelManager.instance.springManager['lines'].forEach(item => item.destroy());
         LevelManager.instance.springManager['lines'] = [];
+        this.word.string = "病毒被消灭了";
     }
 }
 
